384f647a9e531db333fb2507a2cc03b2fd453651
CJonSnow <754472773@qq.com>
2021-11-23 00:20:26 +0800

    1. element-plus配置1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+// import { registerApp } from './global'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// const app = createApp(App)
+// registerApp(app)
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
