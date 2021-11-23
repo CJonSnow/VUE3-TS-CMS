3352919ba1dcb0f90ae1d6b331c1cd230dc209ce
陈风破浪 <754472773@qq.com>
2021-11-23 18:02:04 +0800

    1.axios封装

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import 'element-plus/theme-chalk/index.css'
 import router from './router'
 import store from './store'
 
+import './service/axios_demo'
+
 // const app = createApp(App)
 // registerApp(app)
 createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
