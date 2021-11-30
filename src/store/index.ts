0a1447b2c00e03d48d982652722c7747fc331dda
CJonSnow <754472773@qq.com>
2021-11-30 22:56:59 +0800

    1

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,25 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {},
-  mutations: {},
+interface IRootState {
+  name: string
+  age: number
+}
+// 最好对类型进行限制
+const store = createStore<IRootState>({
+  state: () => {
+    return {
+      name: '小明',
+      age: 18,
+      height: '123'
+    }
+  },
+  mutations: {
+    changeName(state) {
+      console.log(state)
+    }
+  },
   actions: {},
   modules: {}
 })
+
+export default store
